0caac53a18f4a11fdb2a6e8e53779ea301bd5730
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-10-23 15:29:38 -0400

    powerups physics

--- my_game_folder/game.js
+++ my_game_folder/game.js
@@ -2,7 +2,14 @@ const config = {
     width: 256,
     height: 256,
     backgroundColor: 0x00000,
-    scene:[Scene1, Scene2]
+    scene:[Scene1, Scene2],
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug:false
+        }
+    }
+
 }
 
 let game = new Phaser.Game(config);
